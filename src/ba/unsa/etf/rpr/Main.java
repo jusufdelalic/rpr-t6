4676cb9fe3bae8aee30285f66976ba253b1fb2ec
jusufdelalic <jdelalic1@etf.unsa.ba>
2018-11-29 14:21:00 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Main extends Application{
+
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+
+            Parent root = FXMLLoader.load(getClass().getResource("neki_fajl.fxml"));
+            primaryStage.setTitle("Formular");
+            primaryStage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+            primaryStage.show();
+
+        }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
